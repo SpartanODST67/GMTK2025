8cedb624d8440e50852c16ca6c23c455a3f1b71e
Hans Landgrebe <hlxbl20@gmail.com>
2025-08-01 22:04:35 -0500

    fix: clones now properly match transform

--- Spinfoam/Assets/Scripts/MatchTransform.cs
+++ Spinfoam/Assets/Scripts/MatchTransform.cs
@@ -5,7 +5,7 @@ public class MatchTransform : MonoBehaviour
     [SerializeField] Transform copyTransform;
     [SerializeField] Vector3 offset;
 
-    private void Update()
+    private void LateUpdate()
     {
         transform.position = copyTransform.position + offset;
         transform.rotation = copyTransform.rotation;
