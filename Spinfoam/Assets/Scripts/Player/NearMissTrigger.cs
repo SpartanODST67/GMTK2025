1daa065a00e822fd953048f2f25dc33bc226666c
Hans Landgrebe <hlxbl20@gmail.com>
2025-08-02 21:02:15 -0500

    feat: added pitch range to near miss audio

--- Spinfoam/Assets/Scripts/Player/NearMissTrigger.cs
+++ Spinfoam/Assets/Scripts/Player/NearMissTrigger.cs
@@ -13,6 +13,13 @@ public class NearMissTrigger : MonoBehaviour
     [SerializeField] BulletTime bulletTime;
     [SerializeField] float timePerNearMiss = 5f;
     [SerializeField] AudioSource nearMissAudio;
+    float nearMissAudioPitch;
+    [SerializeField] Vector2 nearMissAudioPitchRange;
+
+    private void Start()
+    {
+        nearMissAudioPitch = nearMissAudio.pitch;
+    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
@@ -26,6 +33,7 @@ public class NearMissTrigger : MonoBehaviour
         NearMissText.instance.NearMisses = 1;
         Scorekeeper.instance.AddScore(scorePerNearMiss);
         bulletTime.AddBulletTime(timePerNearMiss);
+        nearMissAudio.pitch = nearMissAudioPitch * Random.Range(nearMissAudioPitchRange.x, nearMissAudioPitchRange.y);
         nearMissAudio.Play();
         isNearMiss = false;
     }
