a57297451e45aab7d6505929f7ba4fcdcf567872
Hans Landgrebe <hlxbl20@gmail.com>
2025-08-02 20:49:13 -0500

    feat: added background music

---
+++ Spinfoam/Assets/Scripts/Systems/Jukebox.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class Jukebox : MonoBehaviour
+{
+    public static Jukebox instance;
+
+    private void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
